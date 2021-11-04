1470286529120d92a8a7e21caad5c93bbfc06a15
perlaagomezz <gomezperla879@gmail.com>
2021-11-04 09:12:37 -0700

    made some changes in my script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log('hello I am here! These are the while loops')
+
+function while1 () {
+    let i = 0
+    while(i<=10){
+        console.log(i*2+1);
+        i++;
+    }
+}
+while1()
+console.log('This is the second while loop')
+function while2() {
+    let i =1
+    while(i<=10){
+    console.log(i*2);
+    i++;
+    }
+}
+while2()
+
+console.log('Hello this is the for loop')
+for(i=1; i<=10; i=i+3){
+console.log(i*2)
+}
+
+console.log('hello this is the 2 for loop')
+for(i=3; i<=9; i=i+1){
+    console.log(i*3)
+}
+
